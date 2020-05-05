b11cea1d1b07c90b8e758e8aa0e69ad415152f9e
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-05-06 01:00:48 +0700

    add "exact" to path to make homepage work !

---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Home = () => {
+  return (
+    <Container className="home-main">
+        <h1>Homepage</h1>
+    </Container>
+  );
+};
+export default Home;
--- src/components/navigation/content.js
+++ src/components/navigation/content.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import { Switch, Route } from "react-router-dom";
+
+import HomeContent from "../home/home"
 import Dashboard from "../map/Dashboard";
 
 const NavContent = () => {
   return (
     <Row className="navcontent">
       <Switch>
+        <Route exact path="/">
+          <HomeContent />
+        </Route>
         <Route path="/map">
           <Dashboard />
         </Route>
