6867fa09381ebf00b5b20f445acae97dc24528d7
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-05-06 19:47:34 +0700

    finish stats route

--- src/components/stats/Dashboard.js
+++ src/components/stats/Dashboard.js
@@ -2,6 +2,9 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import VietnamChart from "./stats-vn";
+import WorldChart from "./stats-world";
+
 import "./Dashboard.css";
 
 const Stats = () => {
@@ -9,6 +12,12 @@ const Stats = () => {
     <Container className="stats-main">
       <Row className="justify-content-center">
         <h1>Statistics</h1>
+      </Row>
+      <Row style={{ marginTop: "30px" }}>
+        <VietnamChart chartName="Vietnam" />
+      </Row>
+      <Row style={{ marginTop: "30px" }}>
+        <WorldChart chartName="Worldwide" />
       </Row>
     </Container>
   );
---
+++ src/components/stats/stats-world.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { Line } from "react-chartjs-2";
+
+const axios = require("axios").default;
+
+const WorldChart = ({ chartName }) => {
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://td.fpt.ai/corona/corona-total.json")
+      .then((response) => {
+        setPatients(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {});
+  }, []);
+
+  const PatientChart = () => (
+    <Container className="justify-content-center">
+      <h3 style={{ textAlign: "center" }}>{chartName}</h3>
+      <Row className="justify-content-center">
+        <Line
+          data={{
+            labels: Object.keys(patients).map((key) => key),
+            datasets: [
+              {
+                data: Object.keys(patients).map((key) => patients[key][0]),
+                label: "Total cases",
+                borderColor: "blue",
+                fill: true,
+              },
+              {
+                data: Object.keys(patients).map((key) => patients[key][1]),
+                label: "Deaths",
+                borderColor: "red",
+                fill: true,
+              },
+              {
+                data: Object.keys(patients).map((key) => patients[key][2]),
+                label: "Recovered",
+                borderColor: "green",
+                fill: true,
+              },
+            ],
+          }}
+        />
+      </Row>
+    </Container>
+  );
+
+  return <PatientChart />;
+};
+
+export default WorldChart;
